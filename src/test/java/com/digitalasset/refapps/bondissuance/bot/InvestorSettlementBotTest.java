34cb18cde05280d89f07f5ec7c178fb1ee1264e9
Levente Molnar <levente.molnar@digitalasset.com>
2019-05-06 14:59:35 +0200

    Adjust InvestorSettlementBot tests

--- src/test/java/com/digitalasset/refapps/bondissuance/bot/InvestorSettlementBotTest.java
+++ src/test/java/com/digitalasset/refapps/bondissuance/bot/InvestorSettlementBotTest.java
@@ -8,9 +8,12 @@ import static com.digitalasset.refapps.bondissuance.bot.BotTestUtils.APP_ID;
 import static com.digitalasset.refapps.bondissuance.bot.BotTestUtils.AUCTION_AGENT;
 import static com.digitalasset.refapps.bondissuance.bot.BotTestUtils.AUCTION_NAME;
 import static com.digitalasset.refapps.bondissuance.bot.BotTestUtils.BOND_ID;
+import static com.digitalasset.refapps.bondissuance.bot.BotTestUtils.CASH_ASSET;
 import static com.digitalasset.refapps.bondissuance.bot.BotTestUtils.CENTRAL_BANK;
 import static com.digitalasset.refapps.bondissuance.bot.BotTestUtils.CSD;
 import static com.digitalasset.refapps.bondissuance.bot.BotTestUtils.INVESTOR;
+import static com.digitalasset.refapps.bondissuance.bot.BotTestUtils.INVESTOR_BOND_ACCOUNT;
+import static com.digitalasset.refapps.bondissuance.bot.BotTestUtils.INVESTOR_CASH_ACCOUNT;
 import static com.digitalasset.refapps.bondissuance.bot.BotTestUtils.ISSUER;
 import static com.digitalasset.refapps.bondissuance.bot.BotTestUtils.TIME_MANAGER;
 import static com.digitalasset.refapps.bondissuance.bot.BotTestUtils.assertHasSingleExercise;
@@ -18,6 +21,10 @@ import static com.digitalasset.refapps.bondissuance.bot.BotTestUtils.assertHasSi
 import com.daml.ledger.javaapi.data.Template;
 import com.daml.ledger.rxjava.components.helpers.CommandsAndPendingSet;
 import com.digitalasset.refapps.bondissuance.LedgerTestView;
+
+import da.finance.fact.asset.AssetDeposit;
+import da.finance.rule.asset.AssetFungible;
+import da.finance.rule.asset.AssetSettlement;
 import da.refapps.bond.lock.AuctionLockedCash;
 import da.refapps.bond.settlement.AuctionSettleRequest;
 import da.refapps.bond.settlement.InvestorSettlementBotTrigger;
@@ -31,61 +38,52 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class InvestorSettlementBotTest {
-//
-//  private InvestorSettlementBot bot;
-//
-//  @Before
-//  public void setupTests() {
-//    bot = new InvestorSettlementBot(TIME_MANAGER, APP_ID, INVESTOR);
-//  }
-//
+
+  private InvestorSettlementBot bot;
+
+  @Before
+  public void setupTests() {
+    bot = new InvestorSettlementBot(TIME_MANAGER, APP_ID, INVESTOR);
+  }
+
   @Test
   public void testTrigger() throws InvocationTargetException, IllegalAccessException {
-//    LedgerTestView<Template> ledgerView = new LedgerTestView<>();
-//    AssetLockedFact.ContractId lockedCashAssetCid = new AssetLockedFact.ContractId("cid-1");
-//    String investorSettlementBotTriggerCid = "investorSettlementBotTriggerCid";
-//    List<AuctionSettleRequest.ContractId> settleRequestCids = Collections.emptyList();
-//
-//    ledgerView.addActiveContract(
-//        AccountFact.TEMPLATE_ID,
-//        "assetAcountFactCid",
-//        new AccountFact(new AccountId(CSD, "acct1", INVESTOR), Collections.emptyList()));
-//    ledgerView.addActiveContract(
-//        AccountFact.TEMPLATE_ID,
-//        "cashAccountFactCid",
-//        new AccountFact(new AccountId(CENTRAL_BANK, "acct2", INVESTOR), Collections.emptyList()));
-//
-//    ledgerView.addActiveContract(
-//        AuctionLockedCash.TEMPLATE_ID,
-//        "auctionLockedCashCid",
-//        new AuctionLockedCash(
-//            INVESTOR, AUCTION_AGENT, AUCTION_NAME, lockedCashAssetCid, Collections.emptyList()));
-//    ledgerView.addActiveContract(
-//        AssetTransferRule.TEMPLATE_ID,
-//        "assetTransferRuleCid",
-//        new AssetTransferRule(CENTRAL_BANK, INVESTOR, Collections.emptyList()));
-//    ledgerView.addActiveContract(
-//        AssetSplitAndMergeRule.TEMPLATE_ID,
-//        "assetSplitAndMergeRuleCid",
-//        new AssetSplitAndMergeRule(CENTRAL_BANK, INVESTOR, Collections.emptyList()));
-//
-//    ledgerView.addActiveContract(
-//        InvestorSettlementBotTrigger.TEMPLATE_ID,
-//        investorSettlementBotTriggerCid,
-//        new InvestorSettlementBotTrigger(
-//            INVESTOR,
-//            ISSUER,
-//            CSD,
-//            AUCTION_AGENT,
-//            AUCTION_NAME,
-//            settleRequestCids,
-//            CENTRAL_BANK,
-//            BOND_ID,
-//            Collections.emptyList()));
-//
-//    CommandsAndPendingSet cmds =
-//        bot.calculateCommands(ledgerView.getRealLedgerView()).blockingFirst();
-//    assertHasSingleExercise(
-//        cmds, investorSettlementBotTriggerCid, "InvestorSettlementBotTrigger_Finalize");
+    LedgerTestView<Template> ledgerView = new LedgerTestView<>();
+    String investorSettlementBotTriggerCid = "investorSettlementBotTriggerCid";
+    List<AuctionSettleRequest.ContractId> settleRequestCids = Collections.emptyList();
+
+
+    ledgerView.addActiveContract(
+        AuctionLockedCash.TEMPLATE_ID,
+        "auctionLockedCashCid",
+        new AuctionLockedCash(
+            INVESTOR, AUCTION_AGENT, AUCTION_NAME, new AssetDeposit(INVESTOR_CASH_ACCOUNT, CASH_ASSET, null), INVESTOR_CASH_ACCOUNT, Collections.emptyList()));
+
+    ledgerView.addActiveContract(
+        AssetFungible.TEMPLATE_ID,
+        "assetFungibleCid",
+        new AssetFungible(INVESTOR_CASH_ACCOUNT, null));
+
+    ledgerView.addActiveContract(
+        AssetSettlement.TEMPLATE_ID,
+        "assetSettlementCid",
+        new AssetSettlement(INVESTOR_CASH_ACCOUNT, null));
+
+    ledgerView.addActiveContract(
+        InvestorSettlementBotTrigger.TEMPLATE_ID,
+        investorSettlementBotTriggerCid,
+        new InvestorSettlementBotTrigger(
+            INVESTOR,
+            ISSUER,
+            AUCTION_AGENT,
+            AUCTION_NAME,
+            settleRequestCids,
+            CENTRAL_BANK,
+            Collections.emptyList()));
+
+    CommandsAndPendingSet cmds =
+        bot.calculateCommands(ledgerView.getRealLedgerView()).blockingFirst();
+    assertHasSingleExercise(
+        cmds, investorSettlementBotTriggerCid, "InvestorSettlementBotTrigger_Finalize");
   }
 }
