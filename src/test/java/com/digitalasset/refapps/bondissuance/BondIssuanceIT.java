e0f37bb69c94bf314ff94a470dee277d468a7954
refapp-machine-da <52401717+refapp-machine-da@users.noreply.github.com>
2019-08-15 12:54:22 +0200

    Upgrade project to latest SDK (#30)

--- src/test/java/com/digitalasset/refapps/bondissuance/BondIssuanceIT.java
+++ src/test/java/com/digitalasset/refapps/bondissuance/BondIssuanceIT.java
@@ -9,7 +9,9 @@ import static org.junit.Assert.assertTrue;
 
 import com.daml.ledger.javaapi.data.Party;
 import com.digitalasset.testing.junit4.Sandbox;
+import com.digitalasset.testing.ledger.DefaultLedgerAdapter;
 import com.digitalasset.testing.utils.ContractWithId;
+import com.google.protobuf.InvalidProtocolBufferException;
 import da.finance.fact.asset.AssetDeposit;
 import da.refapps.bond.auction.*;
 import da.refapps.bond.fixedratebond.FixedRateBondFact;
@@ -30,8 +32,6 @@ import org.junit.rules.ExternalResource;
 
 public class BondIssuanceIT {
   private static final Path RELATIVE_DAR_PATH = Paths.get("./target/bond-issuance.dar");
-  private static final Integer sandboxPort = 6865;
-  private static final int WAIT_TIMEOUT = 20;
   private static final String TEST_MODULE = "DA.RefApps.Bond.Test.MarketSetup";
   private static final String TEST_SCENARIO = "testMarketSetup";
 
@@ -64,7 +64,9 @@ public class BondIssuanceIT {
   @Rule public Sandbox.Process sandbox = sandboxC.process();
 
   @Test
-  public void testFullWorkflow() {
+  public void testFullWorkflow() throws InvalidProtocolBufferException {
+    DefaultLedgerAdapter ledgerAdapter = sandbox.getLedgerAdapter();
+
     // Issuance of a bond
     LocalDate now = LocalDate.now();
     LocalDate maturityDate = now.plusDays(365);
@@ -74,21 +76,18 @@ public class BondIssuanceIT {
     List<LocalDate> couponDates = Arrays.asList();
 
     IssuerRole.ContractId issuerRoleCid =
-        sandbox.getCreatedContractId(
+        ledgerAdapter.getCreatedContractId(
             ISSUER_PARTY, IssuerRole.TEMPLATE_ID, IssuerRole.ContractId::new);
-    sandbox
-        .getLedgerAdapter()
-        .exerciseChoice(
-            ISSUER_PARTY,
-            issuerRoleCid.exerciseIssuerRole_Issuance(
-                3000000L, now, currency, denomination, maturityDate, couponRate, couponDates));
+    ledgerAdapter.exerciseChoice(
+        ISSUER_PARTY,
+        issuerRoleCid.exerciseIssuerRole_Issuance(
+            3000000L, now, currency, denomination, maturityDate, couponRate, couponDates));
     String bondIsin = "BOND-007";
     IssuanceRequest.ContractId issuanceRequest =
-        sandbox.getCreatedContractId(
+        ledgerAdapter.getCreatedContractId(
             CSD_PARTY, IssuanceRequest.TEMPLATE_ID, IssuanceRequest.ContractId::new);
-    sandbox
-        .getLedgerAdapter()
-        .exerciseChoice(CSD_PARTY, issuanceRequest.exerciseIssuanceRequest_Accept(bondIsin));
+    ledgerAdapter.exerciseChoice(
+        CSD_PARTY, issuanceRequest.exerciseIssuanceRequest_Accept(bondIsin));
 
     // Auction commission
     AssetDeposit.ContractId assetDeposit = findBondAssetDeposit(bondIsin);
@@ -96,90 +95,75 @@ public class BondIssuanceIT {
     LocalDate auctionEndDate = auctionStartDate.plusDays(10);
     BigDecimal minPrize = BigDecimal.valueOf(40.0);
     Long size = 1000000L;
-    sandbox
-        .getLedgerAdapter()
-        .exerciseChoice(
-            ISSUER_PARTY,
-            issuerRoleCid.exerciseIssuerRole_CommissionAuction(
-                assetDeposit, auctionStartDate, auctionEndDate, minPrize, size));
+    ledgerAdapter.exerciseChoice(
+        ISSUER_PARTY,
+        issuerRoleCid.exerciseIssuerRole_CommissionAuction(
+            assetDeposit, auctionStartDate, auctionEndDate, minPrize, size));
 
     // Commission, by bot.
     // Acceptance of invitation
     AuctionInvitation.ContractId auctionInvitation =
-        sandbox.getCreatedContractId(
+        ledgerAdapter.getCreatedContractId(
             AGENT_PARTY, AuctionInvitation.TEMPLATE_ID, AuctionInvitation.ContractId::new);
-    sandbox
-        .getLedgerAdapter()
-        .exerciseChoice(
-            AGENT_PARTY, auctionInvitation.exerciseAuctionInvitation_Accept("testAuction"));
+    ledgerAdapter.exerciseChoice(
+        AGENT_PARTY, auctionInvitation.exerciseAuctionInvitation_Accept("testAuction"));
 
     Auction.ContractId auction1 =
-        sandbox.getCreatedContractId(AGENT_PARTY, Auction.TEMPLATE_ID, Auction.ContractId::new);
-    sandbox
-        .getLedgerAdapter()
-        .exerciseChoice(
-            AGENT_PARTY,
-            auction1.exerciseAuction_InviteBidders(
-                Arrays.asList(
-                    BANK1_PARTY.getValue(), BANK2_PARTY.getValue(), BANK3_PARTY.getValue())));
+        ledgerAdapter.getCreatedContractId(
+            AGENT_PARTY, Auction.TEMPLATE_ID, Auction.ContractId::new);
+    ledgerAdapter.exerciseChoice(
+        AGENT_PARTY,
+        auction1.exerciseAuction_InviteBidders(
+            Arrays.asList(BANK1_PARTY.getValue(), BANK2_PARTY.getValue(), BANK3_PARTY.getValue())));
 
-    sandbox
-        .getLedgerAdapter()
-        .setCurrentTime(Instant.ofEpochSecond(auctionEndDate.toEpochDay() * 24 * 60 * 60));
+    ledgerAdapter.setCurrentTime(Instant.ofEpochSecond(auctionEndDate.toEpochDay() * 24 * 60 * 60));
 
     // Bidding
     BidderParticipation.ContractId bidderParticipationBank1 =
-        sandbox.getCreatedContractId(
+        ledgerAdapter.getCreatedContractId(
             BANK1_PARTY, BidderParticipation.TEMPLATE_ID, BidderParticipation.ContractId::new);
-    sandbox
-        .getLedgerAdapter()
-        .exerciseChoice(
-            BANK1_PARTY,
-            bidderParticipationBank1.exerciseBidderParticipation_PlaceBid(
-                BigDecimal.valueOf(42.0), 200000L));
+    ledgerAdapter.exerciseChoice(
+        BANK1_PARTY,
+        bidderParticipationBank1.exerciseBidderParticipation_PlaceBid(
+            BigDecimal.valueOf(42.0), 200000L));
 
     BidderParticipation.ContractId bidderParticipationBank2 =
-        sandbox.getCreatedContractId(
+        ledgerAdapter.getCreatedContractId(
             BANK2_PARTY, BidderParticipation.TEMPLATE_ID, BidderParticipation.ContractId::new);
-    sandbox
-        .getLedgerAdapter()
-        .exerciseChoice(
-            BANK2_PARTY,
-            bidderParticipationBank2.exerciseBidderParticipation_PlaceBid(
-                BigDecimal.valueOf(25.0), 800000L));
+    ledgerAdapter.exerciseChoice(
+        BANK2_PARTY,
+        bidderParticipationBank2.exerciseBidderParticipation_PlaceBid(
+            BigDecimal.valueOf(25.0), 800000L));
 
     BidderParticipation.ContractId bidderParticipationBank3 =
-        sandbox.getCreatedContractId(
+        ledgerAdapter.getCreatedContractId(
             BANK3_PARTY, BidderParticipation.TEMPLATE_ID, BidderParticipation.ContractId::new);
-    sandbox
-        .getLedgerAdapter()
-        .exerciseChoice(
-            BANK3_PARTY,
-            bidderParticipationBank3.exerciseBidderParticipation_PlaceBid(
-                BigDecimal.valueOf(10.0), 800000L));
+    ledgerAdapter.exerciseChoice(
+        BANK3_PARTY,
+        bidderParticipationBank3.exerciseBidderParticipation_PlaceBid(
+            BigDecimal.valueOf(10.0), 800000L));
 
     AuctionBid.ContractId bid =
-        sandbox.getCreatedContractId(
+        ledgerAdapter.getCreatedContractId(
             BANK3_PARTY, AuctionBid.TEMPLATE_ID, AuctionBid.ContractId::new);
     AuctionLockedCash.ContractId lockedCash =
-        sandbox.getCreatedContractId(
+        ledgerAdapter.getCreatedContractId(
             BANK3_PARTY, AuctionLockedCash.TEMPLATE_ID, AuctionLockedCash.ContractId::new);
 
-    sandbox
-        .getLedgerAdapter()
-        .exerciseChoice(
-            BANK3_PARTY,
-            bidderParticipationBank3.exerciseAuctionBidderParticipantion_RevokeLockedBid(
-                bid, lockedCash));
+    ledgerAdapter.exerciseChoice(
+        BANK3_PARTY,
+        bidderParticipationBank3.exerciseAuctionBidderParticipantion_RevokeLockedBid(
+            bid, lockedCash));
 
     // Finalizing auction
     Auction.ContractId auction2 =
-        sandbox.getCreatedContractId(AGENT_PARTY, Auction.TEMPLATE_ID, Auction.ContractId::new);
-    sandbox.getLedgerAdapter().exerciseChoice(AGENT_PARTY, auction2.exerciseAuction_Finalize());
+        ledgerAdapter.getCreatedContractId(
+            AGENT_PARTY, Auction.TEMPLATE_ID, Auction.ContractId::new);
+    ledgerAdapter.exerciseChoice(AGENT_PARTY, auction2.exerciseAuction_Finalize());
 
     // Sanity checks
     ContractWithId<AuctionSettleRequest.ContractId> settleReq1 =
-        sandbox.getMatchedContract(
+        ledgerAdapter.getMatchedContract(
             BANK1_PARTY, AuctionSettleRequest.TEMPLATE_ID, AuctionSettleRequest.ContractId::new);
     assertEquals(
         42L * 200000L,
@@ -189,46 +173,38 @@ public class BondIssuanceIT {
             .longValue());
 
     AuctionParticipantSettleRequest.ContractId apSettleReq1 =
-        sandbox.getCreatedContractId(
+        ledgerAdapter.getCreatedContractId(
             BANK1_PARTY,
             AuctionParticipantSettleRequest.TEMPLATE_ID,
             AuctionParticipantSettleRequest.ContractId::new);
-    sandbox
-        .getLedgerAdapter()
-        .exerciseChoice(BANK1_PARTY, apSettleReq1.exerciseAuctionParticipantSettleRequest_Settle());
+    ledgerAdapter.exerciseChoice(
+        BANK1_PARTY, apSettleReq1.exerciseAuctionParticipantSettleRequest_Settle());
 
     ContractWithId<AuctionParticipantSettleRequest.ContractId> apSettleReq2 =
-        sandbox.getMatchedContract(
+        ledgerAdapter.getMatchedContract(
             BANK2_PARTY,
             AuctionParticipantSettleRequest.TEMPLATE_ID,
             AuctionParticipantSettleRequest.ContractId::new);
     assertTrue(
         AuctionParticipantSettleRequest.fromValue(apSettleReq2.record).settleRequestCids.isEmpty());
-    sandbox
-        .getLedgerAdapter()
-        .exerciseChoice(
-            BANK2_PARTY, apSettleReq2.contractId.exerciseAuctionParticipantSettleRequest_Settle());
+    ledgerAdapter.exerciseChoice(
+        BANK2_PARTY, apSettleReq2.contractId.exerciseAuctionParticipantSettleRequest_Settle());
 
     // Requesting redemption at CSD
-    sandbox
-        .getLedgerAdapter()
-        .setCurrentTime(Instant.ofEpochSecond(maturityDate.toEpochDay() * 24 * 60 * 60));
+    ledgerAdapter.setCurrentTime(Instant.ofEpochSecond(maturityDate.toEpochDay() * 24 * 60 * 60));
     FixedRateBondFact.ContractId fixedRateBondFact =
-        sandbox.getCreatedContractId(
+        ledgerAdapter.getCreatedContractId(
             ISSUER_PARTY, FixedRateBondFact.TEMPLATE_ID, FixedRateBondFact.ContractId::new);
-    sandbox
-        .getLedgerAdapter()
-        .exerciseChoice(ISSUER_PARTY, issuerRoleCid.exerciseIssuerRole_Redeem(fixedRateBondFact));
+    ledgerAdapter.exerciseChoice(
+        ISSUER_PARTY, issuerRoleCid.exerciseIssuerRole_Redeem(fixedRateBondFact));
 
     RedemptionRequest.ContractId redemptionRequest =
-        sandbox.getCreatedContractId(
+        ledgerAdapter.getCreatedContractId(
             CSD_PARTY, RedemptionRequest.TEMPLATE_ID, RedemptionRequest.ContractId::new);
-    sandbox
-        .getLedgerAdapter()
-        .exerciseChoice(CSD_PARTY, redemptionRequest.exerciseRedemptionRequest_Accept());
+    ledgerAdapter.exerciseChoice(CSD_PARTY, redemptionRequest.exerciseRedemptionRequest_Accept());
 
     assertTrue(
-        sandbox.observeMatchingContracts(
+        ledgerAdapter.observeMatchingContracts(
             BANK1_PARTY,
             AssetDeposit.TEMPLATE_ID,
             AssetDeposit::fromValue,
@@ -243,8 +219,10 @@ public class BondIssuanceIT {
     while (true) {
       // This will fail, after there are no more asset deposit contracts
       ContractWithId<AssetDeposit.ContractId> assetDeposit =
-          sandbox.getMatchedContract(
-              ISSUER_PARTY, AssetDeposit.TEMPLATE_ID, AssetDeposit.ContractId::new);
+          sandbox
+              .getLedgerAdapter()
+              .getMatchedContract(
+                  ISSUER_PARTY, AssetDeposit.TEMPLATE_ID, AssetDeposit.ContractId::new);
       if (AssetDeposit.fromValue(assetDeposit.record).asset.id.label.equals(expectedLabel)) {
         return assetDeposit.contractId;
       }
