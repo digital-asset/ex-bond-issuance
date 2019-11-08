c95dcdf2099803ef930d04aa980be0b6f1c38e81
Richard Kapolnai <richard.kapolnai@digitalasset.com>
2019-11-08 11:41:42 +0100

    ERA-307 use finlib as dar package instead of copied source (#64)
    * ERA-307 use finlib as dar package instead of copied source
    
    * license header
    
    * Ignore license check in .daml extracted from dar
    
    * Call genfinlib in daml_test circleci goal
    
    * install python3
    
    * sudo
    
    * workaround experiment
    
    * jdk 14
    
    * Revert "jdk 14"
    
    This reverts commit 5854d826c4619b9bbec61d1cd80fdf92eab8bc7e.
    
    * Revert "workaround experiment"
    
    This reverts commit dfa300555365101a34fe33c40e748a55c38d0095.
    
    * Revert "sudo"
    
    This reverts commit cf98abbf4d1568de92b7cef0ba9c981b960581f1.
    
    * Revert "install python3"
    
    This reverts commit ddf839fb2183421bf05a33a4aed309276d0af32e.
    
    * switch to python2
    
    * use python2 in circleci
    
    * use python2 in pom.xml
    
    * rewrite python imports
    
    * enforce python2
    
    * Add Python2 as a requirement
    
    * Remove backtick in .circleci/config.yml
    
    Co-Authored-By: Tamás Kálcza <tamas.kalcza@digitalasset.com>
    
    * Hardcode dar filename, minor improvements
    
    * log info level
    
    * call python instead of python2
    
    * support both python2 and python3
    
    * call script getfinlib.py directly
    
    * a new attempt for python 2/3 compatibility
    
    * remove unused import requests

--- README.md
+++ README.md
@@ -17,6 +17,7 @@ Be sure you have the following installed:
 - Docker
 - Java
 - Maven
+- Python
 
 #### Build with Maven
 
---
+++ scripts/getfinlib.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2019, Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Produces dar file `target/finlib-master-sdk-{daml_sdk_version}.dar' by fetching the source and building it.
+# Arguments: daml_sdk_version
+
+from io import BytesIO
+from os import environ, getcwd, path
+from shutil import rmtree
+from subprocess import call
+from sys import argv
+from tempfile import mkdtemp
+from zipfile import ZipFile
+import logging
+
+
+# based on 'Cheat Sheet: Writing Python 2-3 compatible code'
+# from https://python-future.org/compatible_idioms.html
+try:
+    from urllib.request import urlopen
+except ImportError:
+    from urllib2 import urlopen
+
+# Currently there is no release yet, so use master
+url = 'https://github.com/digital-asset/lib-finance/archive/master.zip'
+
+
+def get_source(url, tmp_directory):
+    logging.info('Unzipping {url}'.format(url=url))
+    resp = urlopen(url)
+    with ZipFile(BytesIO(resp.read())) as zipfile:
+        zipfile.extractall(tmp_directory)
+
+
+def build_dar(daml_sdk_version, full_path_to_dar, tmp_directory):
+    extracted_directory = 'lib-finance-master'
+    build_command = 'daml build --project-root {tmp_directory}/{extracted_directory}/ -o {full_path_to_dar}'.format(
+        tmp_directory=tmp_directory, extracted_directory=extracted_directory, full_path_to_dar=full_path_to_dar)
+    logging.info(
+        'Executing {build_command} with DAML_SDK_VERSION={daml_sdk_version}'.format(
+            build_command=build_command, daml_sdk_version=daml_sdk_version))
+    environ['DAML_SDK_VERSION'] = daml_sdk_version
+    call(build_command.split(' '))
+
+
+logging.basicConfig(level=logging.INFO)
+
+daml_sdk_version = argv[1]
+full_path_to_dar = '{pwd}/target/finlib-master-sdk-{daml_sdk_version}.dar'.format(
+    pwd=getcwd(), daml_sdk_version=daml_sdk_version)
+
+if path.exists(full_path_to_dar):
+    logging.info('{full_path_to_dar} already exists, nothing to do'.format(full_path_to_dar=full_path_to_dar))
+    exit(0)
+else:
+    logging.info('{full_path_to_dar} will be created'.format(full_path_to_dar=full_path_to_dar))
+
+tmp_directory = mkdtemp()
+logging.debug('Working in {tmp_directory}'.format(tmp_directory=tmp_directory))
+
+get_source(url, tmp_directory)
+
+build_dar(daml_sdk_version, full_path_to_dar, tmp_directory)
+
+logging.debug('Removing {tmp_directory}'.format(tmp_directory=tmp_directory))
+rmtree(tmp_directory)
