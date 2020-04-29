7f2de6552dd6db7a37b50d8df98168ad7816088f
chris norris <chris.norris@digitalasset.com>
2020-04-29 20:14:02 +0100

    Commence customization of columns per page

--- ui-js/src/pages/balance/Balance.js
+++ ui-js/src/pages/balance/Balance.js
@@ -1,12 +1,26 @@
 import React from "react";
 import Contracts from "../../components/Contracts/Contracts";
 import { useQuery} from "@daml/react";
+import {Grid} from '@material-ui/core';
 
 import { AssetDeposit } from "@daml2ts/finlib-1.0.0/lib/DA/Finance/Fact/Asset";
 export default function Report() {
 
-  const reviews = useQuery(AssetDeposit);
-  // const exerciseGive = useExercise(Asset.Give);
+  const deposits = useQuery(AssetDeposit);
 
-  return (<Contracts contracts={reviews.contracts}/>);
-}
+  return (
+    <>
+     <Grid item></Grid>
+     {/* <Contracts contracts={deposits.contracts}/> */}
+      <Contracts
+        contracts={deposits.contracts}
+        columns={[
+          ["Owner", "payload.account.owner"],
+          ["Account", "payload.account.id.label"],
+          ["Account Provider", "payload.account.provider"],
+          ["Quantity", "payload.asset.quantity"],
+          ["Asset Id", "payload.asset.id.label"]
+        ]} />
+        </>
+      );
+    }
