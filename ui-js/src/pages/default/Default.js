9d28338bf14b8d906123f16e5a09d3cc2d02df51
chris norris <chris.norris@digitalasset.com>
2020-04-29 20:14:56 +0100

    General contracts page to be implemented following discuss query reponse

--- ui-js/src/pages/default/Default.js
+++ ui-js/src/pages/default/Default.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Contracts from "../../components/Contracts/Contracts";
-import { useQuery } from "@daml/react";
+// import Contracts from "../../components/Contracts/Contracts";
+// import { useQuery } from "@daml/react";
 
 // https://discuss.daml.com/t/usequery-in-the-daml-react-library-forces-me-to-pass-in-a-template/296
 
