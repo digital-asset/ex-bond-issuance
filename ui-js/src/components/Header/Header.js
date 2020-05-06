b5a6ec36ff5922ff4d2a632a7e0c50fcdeaa0a52
Tamás Kálcza <tamas.kalcza@digitalasset.com>
2020-05-06 11:19:16 +0200

    Fix license headers.

--- ui-js/src/components/Header/Header.js
+++ ui-js/src/components/Header/Header.js
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2019, Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 import React from "react";
 import { withRouter } from "react-router-dom";
 import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core";
--- ui-js/src/serviceWorker.js
+++ ui-js/src/serviceWorker.js
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2019, Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 // This optional code is used to register a service worker.
 // register() is not called by default.
 
