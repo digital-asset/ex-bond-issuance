4a82845f7626f3165bca24284cbe08b43b2e698d
Tamás Kálcza <tamas.kalcza@digitalasset.com>
2020-05-08 01:59:49 +0200

    Finetuned ledger waiter script.

--- scripts/waitForLedger.sh
+++ scripts/waitForLedger.sh
@@ -5,8 +5,9 @@
 #
 
 wait_for_ledger() {
-  local host="$1"
-  local port="$2"
+  local host="${1:?Ledger host must be specified.}"
+  local port="${2:?Ledger port must be specified.}"
+  echo "Ledger URL: http://$host:$port"
   until nc -z "$host" "$port"; do
     echo "Waiting for ledger..."
     sleep 1
@@ -14,7 +15,4 @@ wait_for_ledger() {
   echo "Connected to ledger."
 }
 
-ledger_host="$1"
-ledger_port="$2"
-
-wait_for_ledger "$ledger_host" "$ledger_port"
+wait_for_ledger "$1" "$2"
