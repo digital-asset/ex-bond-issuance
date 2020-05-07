c179a96db080779075aff7ef94e4d7d4ed740ad7
gaborh-da <33900822+gaborh-da@users.noreply.github.com>
2020-05-07 14:36:39 +0200

    Add AuctionFinalize trigger (#97)
    * Add AuctionFinalize trigger
    
    * Fix headers
    
    * Add Commission trigger
    
    * Fix function name
    
    * Make Auction Finalize simpler
    
    * Simplify Commission trigger
    
    * Skeleton for investor settlement trigger.
    
    * Added skeletons for utility functions from Java bot.
    
    * Add Place bid trigger
    
    * Fix comments
    
    * Implemented main functionality of settlement trigger.
    
    * More logical code arrangement.
    
    * Implemented findAssetSettlement and findAssetFugible.
    
    * Implemented findAuctionLockedCashes.
    
    * Unified the API of find functions.
    
    * Extracted common code.
    
    * Simplified imports.
    
    * Better names.
    
    * Simplified helper function API.
    
    * Simplified InvestorSettlementBotTrigger template name.
    
    * Moved error handling into one place.
    
    * Flipped arguments.
    
    * Using fmap to simplify implementations.
    
    * Simplifications.
    
    * Started to extract common functionality to Util.
    
    * Implemented headNote in terms of stdlib functions.
    
    * Made utils more generic.
    
    * Reduced duplication in commission trigger.
    
    * Fixed typo.
    
    * Simplified code.
    
    * Added test for investor settlement trigger.
    
    * Extend test case with noop trigger run.
    
    * Generalised find methods.
    
    * Code reduce.
    
    * Added typeclasses to find assets.
    
    * Removed unnecessary typeclasses, simplified implementations.
    
    * Add redemption calculation trigger
    
    * Use contract utils
    
    * Fixed license header style in Java files.
    
    * Add redemption finalize trigger
    
    * ERA-792 Create a trigger process runner (Java) (#99)
    
    * Draft implementation of a trigger runner.
    
    * Added logic to wait trigger start.
    
    * Fixed formatting.
    
    * Extracted waiting logic.
    
    * ERA-793 scripts in docker (#101)
    
    * Merge navigator and sandbox containers.
    
    * Simplified license plugin for Docker files.
    
    * Sandbox container uses 'daml start'.
    
    * Init script is used by 'daml start'.
    
    * Added finlib to sandbox container.
    
    * Removed unnecessary import.
    
    * Commented allocation.
    
    * Add a trigger runner
    
    * Fixed permission issues in Docker.
    
    * Logging ledger setup.
    
    * Simplified trigger runner script.
    
    * Limiting VM sizes for triggers.
    
    * Remove unused bots
    
    * Fixed missing signatures in DAML files.
    
    * Attempt to fix Docker issue with 'daml start' on Linux machines.
    
    * Revert "Attempt to fix Docker issue with 'daml start' on Linux machines."
    
    This reverts commit c595265f68451acf3c20fb1b8c49d3d7beecb626.
    
    * Attempt to fix docker.
    
    * Fix formatting in BondIssuanceIT
    
    * Limit trigger VM size.
    
    * Fixed log file names.
    
    * Increased CI resource size.
    
    * Fixed formatting.
    
    Co-authored-by: Tamás Kálcza <tamas.kalcza@digitalasset.com>

---
+++ scripts/startTriggers.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2019, Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+cleanup() {
+    pids=$(jobs -p)
+    echo Killing "$pids"
+    [ -n "$pids" ] && kill $pids
+}
+
+trap "cleanup" INT QUIT TERM
+
+if [ $# -lt 2 ]; then
+    echo "${0} SANDBOX_HOST SANDBOX_PORT [DAR_FILE]"
+    exit 1
+fi
+
+SANDBOX_HOST="${1}"
+SANDBOX_PORT="${2}"
+DAR_FILE="${3:-/home/daml/bond-issuance.dar}"
+
+run_trigger() {
+  local trigger_name="$1"
+  local party="$2"
+  daml trigger \
+      --wall-clock-time \
+      --dar "${DAR_FILE}" \
+      --trigger-name "$trigger_name" \
+      --ledger-host "${SANDBOX_HOST}" \
+      --ledger-port "${SANDBOX_PORT}" \
+      --ledger-party "$party"
+}
+
+run_trigger DA.RefApps.Bond.Triggers.InvestorSettlementTrigger:investorSettlementTrigger Bank1 &
+run_trigger DA.RefApps.Bond.Triggers.PlaceBidTrigger:placeBidTrigger Bank1 &
+run_trigger DA.RefApps.Bond.Triggers.InvestorSettlementTrigger:investorSettlementTrigger Bank2 &
+run_trigger DA.RefApps.Bond.Triggers.PlaceBidTrigger:placeBidTrigger Bank2 &
+run_trigger DA.RefApps.Bond.Triggers.InvestorSettlementTrigger:investorSettlementTrigger Bank3 &
+run_trigger DA.RefApps.Bond.Triggers.PlaceBidTrigger:placeBidTrigger Bank3 &
+run_trigger DA.RefApps.Bond.Triggers.CommissionTrigger:commissionTrigger Issuer &
+run_trigger DA.RefApps.Bond.Triggers.RedemptionFinalizeTrigger:redemptionFinalizeTrigger Issuer &
+run_trigger DA.RefApps.Bond.Triggers.AuctionFinalizeTrigger:auctionFinalizeTrigger AuctionAgent &
+run_trigger DA.RefApps.Bond.Triggers.RedemptionCalculationTrigger:redemptionCalculationTrigger CSD &
+
+sleep 2
+pids=$(jobs -p)
+echo Waiting for $pids
+wait $pids
