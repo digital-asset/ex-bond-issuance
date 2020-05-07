82b66db60e294291fe962420917ef329dbb12d88
Tamás Kálcza <tamas.kalcza@digitalasset.com>
2020-05-07 14:04:36 +0200

    Removed unnecessary function in trigger runner script.

--- scripts/startTrigger.sh
+++ scripts/startTrigger.sh
@@ -4,35 +4,20 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-require() {
-  local variable="$1"
-  local message="$2"
-  if [[ -z "$variable" ]]; then
-    echo "$message"
-    exit 1
-  fi
-}
-
 run_trigger() {
-  local trigger_name="$1"
-  local party="$2"
+  local ledger_host="${LEDGER_HOST:?Ledger host must be set.}"
+  local ledger_port="${LEDGER_PORT:?Ledger port must be set.}"
+  local dar_file="${DAR_FILE:?DAR file must be set.}"
+  local trigger_name="${1:?Trigger name must be set.}"
+  local party="${2:?Party name must be set.}"
+
   daml trigger \
     --wall-clock-time \
-    --dar "${DAR_FILE}" \
-    --ledger-host "${LEDGER_HOST}" \
-    --ledger-port "${LEDGER_PORT}" \
+    --dar "$dar_file" \
+    --ledger-host "$ledger_host" \
+    --ledger-port "$ledger_port" \
     --trigger-name "$trigger_name" \
     --ledger-party "$party"
 }
 
-require "${LEDGER_HOST}" "LEDGER_HOST must be set."
-require "${LEDGER_PORT}" "LEDGER_PORT must be set."
-require "${DAR_FILE}" "DAR_FILE must be set."
-
-trigger_name="$1"
-party="$2"
-
-require "$trigger_name" "Trigger name must be set as first argument."
-require "$party" "Party name must be set as second argument."
-
-run_trigger "$trigger_name" "$party"
+run_trigger "$1" "$2"
