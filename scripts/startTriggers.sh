a09ee1a9b3b8f949428b617088cc43fcf80c7a03
Gabor Hosszu <gabor@digitalasset.com>
2020-05-13 14:50:54 +0200

    Grep party names

--- scripts/startTriggers.sh
+++ scripts/startTriggers.sh
@@ -23,9 +23,12 @@ SANDBOX_HOST="${1}"
 SANDBOX_PORT="${2}"
 DAR_FILE="${3:-/home/daml/bond-issuance.dar}"
 
+LIST_PARTIES_RESPONSE="$(daml ledger list-parties --host "${SANDBOX_HOST}" --port "${SANDBOX_PORT}" | tail -n +2)"
+PARTIES="$(echo "${LIST_PARTIES_RESPONSE}" | cut -d"{" -f2 | cut -d" " -f3 | cut -d"'" -f2)"
+
 run_trigger() {
   local trigger_name="$1"
-  local party="$2"
+  local party="$(get_party "$2")"
   daml trigger \
       --wall-clock-time \
       --dar "${DAR_FILE}" \
@@ -33,6 +36,10 @@ run_trigger() {
       --ledger-host "${SANDBOX_HOST}" \
       --ledger-port "${SANDBOX_PORT}" \
       --ledger-party "$party"
+}
+
+get_party() {
+    echo "$PARTIES" | grep "${1}"
 }
 
 run_trigger DA.RefApps.Bond.Triggers.InvestorSettlementTrigger:investorSettlementTrigger Bank1 &
