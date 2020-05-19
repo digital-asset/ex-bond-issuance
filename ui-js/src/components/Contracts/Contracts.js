3698df27652518135bacfb83502afec696fc4adb
Gabor Hosszu <gabor@digitalasset.com>
2020-05-19 15:20:03 +0200

    Fix header

--- ui-js/src/components/Contracts/Contracts.js
+++ ui-js/src/components/Contracts/Contracts.js
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2019, Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 import React, { useState } from "react";
 import ReactJson from "react-json-view";
 import { Dialog, DialogActions, DialogContent, DialogTitle, FormControl, InputLabel, Select, MenuItem, Grid, Table, TableHead, TableRow, TableCell, TableBody, TextField, Button } from "@material-ui/core";
