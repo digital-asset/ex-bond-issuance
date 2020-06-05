2083e263113c8d248e03035259b1caccdbb90b84
Tamás Kálcza <tamas.kalcza@digitalasset.com>
2020-06-05 13:49:45 +0200

    ERA-826 Prevent using USD as a bond (#120)
    * Fixed Docker: removed non-existing file from COPY.
    
    * Draft implementation of filtering asset deposits.
    
    * Display quantity along with asset id.
    
    * Applied formatting.

--- ui-js/src/pages/issuerRole/IssuerRole.js
+++ ui-js/src/pages/issuerRole/IssuerRole.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2019, Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
-import React from "react";
+import React, { useMemo } from "react";
 import Contracts from "../../components/Contracts/Contracts";
 import { field } from "../../components/Contracts/Contracts";
 import { useStreamQuery, useLedger } from "@daml/react";
@@ -24,7 +24,7 @@ export default function Report() {
   const denomination = "Denomination"
   const maturityDate = "Maturity date"
   const couponRate = "Coupon rate"
-  const doIssue = function(contract, params) {
+  const doIssue = function (contract, params) {
     const payload = {
       issueSize: params[issueSize],
       issueDate: params[issueDate],
@@ -44,7 +44,7 @@ export default function Report() {
   const endDate = "End date"
   const minPrice = "Minimum price"
   const size = "Size"
-  const doStartAuction = function(contract, params) {
+  const doStartAuction = function (contract, params) {
     const payload = {
       bondAssetDepositCid: params[bondAssetDepositCid],
       startDate: params[startDate],
@@ -56,13 +56,17 @@ export default function Report() {
   };
 
   const fixedRateBondFactCid = "Fixed rate bond"
-  const doRedeem = function(contract, params) {
+  const doRedeem = function (contract, params) {
     const payload = {
       fixedRateBondFactCid: params[fixedRateBondFactCid]
     }
     ledger.exercise(IssuerRole.IssuerRole_Redeem, contract.contractId, payload);
   };
 
+  const filteredAssets = useMemo(
+    () => filterByFixedRateBondFact(assetDeposits.contracts, fixedRateBondFacts.contracts),
+    [assetDeposits, fixedRateBondFacts]
+  );
   return (<Contracts contracts={roles.contracts}
     columns={[["Contract Id", "contractId"],
 
@@ -72,25 +76,43 @@ export default function Report() {
     dialogs={[
       ["Issue bond",
         [field(issueSize, "number"),
-         field(issueDate, "date"),
-         field(currency, "menu", ["USD"]),
-         field(denomination, "number"),
-         field(maturityDate, "date"),
-         field(couponRate, "number")],
-         doIssue
+        field(issueDate, "date"),
+        field(currency, "menu", ["USD"]),
+        field(denomination, "number"),
+        field(maturityDate, "date"),
+        field(couponRate, "number")],
+        doIssue
       ],
       ["Commission auction",
-        [field(bondAssetDepositCid, "menu", assetDeposits.contracts.map(c => c.contractId), assetDeposits.contracts.map(c => c.payload.asset.id.label)),
-         field(startDate, "date"),
-         field(endDate, "date"),
-         field(minPrice, "number"),
-         field(size, "number")],
-         doStartAuction
+        [field(bondAssetDepositCid, "menu", filteredAssets.map(c => c.contractId), filteredAssets.map(displayAssetDeposit)),
+        field(startDate, "date"),
+        field(endDate, "date"),
+        field(minPrice, "number"),
+        field(size, "number")],
+        doStartAuction
       ],
       ["Redeem",
         [field(fixedRateBondFactCid, "menu", fixedRateBondFacts.contracts.map(c => c.contractId), fixedRateBondFacts.contracts.map(c => c.payload.isin))],
-         doRedeem
+        doRedeem
       ]
     ]}
   />);
+}
+
+function hasSameIsin(assetDeposit, fixedRateBondFact) {
+  return fixedRateBondFact.payload.isin === assetDeposit.payload.asset.id.label;
+}
+
+function isinExistsIn(assetDeposit, fixedRateBondFacts) {
+  return fixedRateBondFacts.some(fixedRateBondFact => hasSameIsin(assetDeposit, fixedRateBondFact));
+}
+
+function filterByFixedRateBondFact(assetDeposits, fixedRateBondFacts) {
+  console.log(`Asset deposits: ${JSON.stringify(assetDeposits)}`);
+  console.log(`Fixed rate bond facts: ${JSON.stringify(fixedRateBondFacts)}`);
+  return assetDeposits.filter(x => isinExistsIn(x, fixedRateBondFacts));
+}
+
+function displayAssetDeposit(assetDeposit) {
+  return `${assetDeposit.payload.asset.id.label} (Quantity: ${assetDeposit.payload.asset.quantity})`;
 }
