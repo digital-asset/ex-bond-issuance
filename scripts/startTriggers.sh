72336e03f4ed5bac84bddb2d7ebabbd9242bb852
Gabor Hosszu <gabor@digitalasset.com>
2020-07-07 08:55:06 +0200

    Use trigger service

--- scripts/startTriggers.sh
+++ scripts/startTriggers.sh
@@ -22,29 +22,54 @@ fi
 SANDBOX_HOST="${1}"
 SANDBOX_PORT="${2}"
 DAR_FILE="${3:-/home/daml/bond-issuance.dar}"
+DEFAULT_SERVICE_PORT="8088"
 
-run_trigger() {
-  local trigger_name="$1"
-  local party="$2"
-  daml trigger \
-      --wall-clock-time \
-      --dar "${DAR_FILE}" \
-      --trigger-name "$trigger_name" \
-      --ledger-host "${SANDBOX_HOST}" \
-      --ledger-port "${SANDBOX_PORT}" \
-      --ledger-party "$party"
+start_trigger() {
+  local pkg_id="$1"
+  local trigger_name="$2"
+  local party="$3"
+  curl --user ${party}':secret' \
+     -X POST localhost:8088/v1/start \
+     -H "Content-type: application/json" -H "Accept: application/json" \
+     -d '{"triggerName":"'${pkg_id}:${trigger_name}'"}'
 }
 
-run_trigger DA.RefApps.Bond.Triggers.InvestorSettlementTrigger:investorSettlementTrigger Bank1 &
-run_trigger DA.RefApps.Bond.Triggers.PlaceBidTrigger:placeBidTrigger Bank1 &
-run_trigger DA.RefApps.Bond.Triggers.InvestorSettlementTrigger:investorSettlementTrigger Bank2 &
-run_trigger DA.RefApps.Bond.Triggers.PlaceBidTrigger:placeBidTrigger Bank2 &
-run_trigger DA.RefApps.Bond.Triggers.InvestorSettlementTrigger:investorSettlementTrigger Bank3 &
-run_trigger DA.RefApps.Bond.Triggers.PlaceBidTrigger:placeBidTrigger Bank3 &
-run_trigger DA.RefApps.Bond.Triggers.CommissionTrigger:commissionTrigger Issuer &
-run_trigger DA.RefApps.Bond.Triggers.RedemptionFinalizeTrigger:redemptionFinalizeTrigger Issuer &
-run_trigger DA.RefApps.Bond.Triggers.AuctionFinalizeTrigger:auctionFinalizeTrigger AuctionAgent &
-run_trigger DA.RefApps.Bond.Triggers.RedemptionCalculationTrigger:redemptionCalculationTrigger CSD &
+wait_for_service() {
+  local host="$1"
+  local port="$2"
+  until nc -z "$host" "$port"; do
+    echo "Waiting for service..."
+    sleep 3
+  done
+  echo "Connected to service."
+}
+
+# We grep package ID from damlc DAR inspection output
+# It is line that starts with the DAR name and containts the package ID between quotation marks
+DAR_FILE_BASENAME="${DAR_FILE##*/}"
+DAR_NAME="${DAR_FILE_BASENAME%%.*}"
+PACKAGE_ID=$(daml damlc inspect-dar "${DAR_FILE}" | grep "${DAR_NAME}" | grep "\".*\"" | cut -d'"' -f 2)
+echo "Package ID: ${PACKAGE_ID}"
+
+echo "Starting trigger service."
+daml trigger-service \
+    --ledger-host "${SANDBOX_HOST}" \
+    --ledger-port "${SANDBOX_PORT}" \
+    --wall-clock-time --no-secret-key --dar ${DAR_FILE} &
+
+wait_for_service ${SANDBOX_HOST} ${DEFAULT_SERVICE_PORT}
+
+echo "Starting triggers."
+start_trigger ${PACKAGE_ID} DA.RefApps.Bond.Triggers.InvestorSettlementTrigger:investorSettlementTrigger Bank1 &
+start_trigger ${PACKAGE_ID} DA.RefApps.Bond.Triggers.PlaceBidTrigger:placeBidTrigger Bank1 &
+start_trigger ${PACKAGE_ID} DA.RefApps.Bond.Triggers.InvestorSettlementTrigger:investorSettlementTrigger Bank2 &
+start_trigger ${PACKAGE_ID} DA.RefApps.Bond.Triggers.PlaceBidTrigger:placeBidTrigger Bank2 &
+start_trigger ${PACKAGE_ID} DA.RefApps.Bond.Triggers.InvestorSettlementTrigger:investorSettlementTrigger Bank3 &
+start_trigger ${PACKAGE_ID} DA.RefApps.Bond.Triggers.PlaceBidTrigger:placeBidTrigger Bank3 &
+start_trigger ${PACKAGE_ID} DA.RefApps.Bond.Triggers.CommissionTrigger:commissionTrigger Issuer &
+start_trigger ${PACKAGE_ID} DA.RefApps.Bond.Triggers.RedemptionFinalizeTrigger:redemptionFinalizeTrigger Issuer &
+start_trigger ${PACKAGE_ID} DA.RefApps.Bond.Triggers.AuctionFinalizeTrigger:auctionFinalizeTrigger AuctionAgent &
+start_trigger ${PACKAGE_ID} DA.RefApps.Bond.Triggers.RedemptionCalculationTrigger:redemptionCalculationTrigger CSD &
 
 sleep 2
 pids=$(jobs -p)
