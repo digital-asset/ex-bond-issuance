f41b4010f6ed5b56b0b8581299bf6cd8d92c9e17
chris norris <chris.norris@digitalasset.com>
2021-04-16 18:58:15 +0100

    Script to extract parties from participants

---
+++ ui/src/utils/gen-ledger-parties.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const participants = require('../participants.json');
+
+var stream = fs.createWriteStream("ledger-parties.json");
+
+// "chrisNorris",
+var desiredParties = [ "auctionAgent", "csd", "bank1", "bank2", "bank3", "issuer", "centralBank", "regulator", "operator"];
+
+stream.once('open', function() {
+  stream.write(JSON.stringify(Object.fromEntries(Object.entries(participants.party_participants)
+  .filter(participant => desiredParties.includes(participant[1]))
+  .map(participant => [participant[1], participant[0]])
+  )));
+  stream.end();
+});
