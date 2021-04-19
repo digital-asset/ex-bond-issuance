39d05abb74a25d279302fc3b6d6e78deb2953874
chris norris <chris.norris@digitalasset.com>
2021-04-19 20:07:11 +0100

    Add licence header

--- ui/src/components/Util.js
+++ ui/src/components/Util.js
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2019, Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 import { CreateEvent } from '@daml/ledger';
 import participants from '../participants.json';
 
