9fa6128cfd11c13fee994cbf6a48b5c89c92348a
chris norris <chris.norris@digitalasset.com>
2021-04-30 20:20:33 +0100

    Migrate index

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -2,16 +2,15 @@
  * Copyright (c) 2019, Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
+import { CssBaseline } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/styles";
 import React from "react";
 import ReactDOM from "react-dom";
-import { ThemeProvider } from "@material-ui/styles";
-import { CssBaseline } from "@material-ui/core";
-
-import Themes from "./themes";
 import App from "./components/App";
-import * as serviceWorker from "./serviceWorker";
 import { LayoutProvider } from "./context/LayoutContext";
 import { UserProvider } from "./context/UserContext";
+import Themes from "./themes";
+
 
 ReactDOM.render(
   <LayoutProvider>
@@ -24,8 +23,3 @@ ReactDOM.render(
   </LayoutProvider>,
   document.getElementById("root"),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
