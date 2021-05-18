f99255fcb1e75c09447a6e0c2e05efab42a298da
chris norris <chris.norris@digitalasset.com>
2021-05-18 17:48:39 +0100

    Complete merge

--- scripts/getfinlib.py
+++ scripts/getfinlib.py
@@ -7,15 +7,14 @@
 # Produces dar file `target/finlib-master-sdk-{daml_sdk_version}.dar' by fetching the source and building it.
 # Arguments: daml_sdk_version
 
+import logging
 from io import BytesIO
 from os import environ, getcwd, path, chdir, chmod, path, mkdir
 from platform import system
-from shutil import rmtree
 from subprocess import call
 from sys import argv
 from tempfile import mkdtemp
 from zipfile import ZipFile
-import logging
 from shutil import copyfile
 
 # based on 'Cheat Sheet: Writing Python 2-3 compatible code'
@@ -57,17 +56,16 @@ def build_dar(daml_sdk_version, full_path_to_dar, tmp_directory):
     logging.info(
         'Executing {build_command} with DAML_SDK_VERSION={daml_sdk_version}'.format(
             build_command=" ".join(build_command), daml_sdk_version=daml_sdk_version))
-    logging.info('Setting environment to {d}'.format(d=daml_sdk_version))
+    logging.info('Setting environment to {sdk}'.format(sdk=daml_sdk_version))
     environ['DAML_SDK_VERSION'] = daml_sdk_version
     logging.info('Calling to {build_command}')
-    # call(build_command)
     chdir(project_root)
 
     chmod( './build.sh', 0o775)
-    call([ './build.sh', 'build-dars'])
+    call(build_command)
     if not path.exists(target_dir):
         mkdir(target_dir)
-    logging.info('Copying to {dest}'.format(dest=full_path_to_dar))
+    logging.info('Copying to {full_path}'.format(full_path=full_path_to_dar))
     copyfile('./model/.daml/dist/finlib-2.0.0.dar', full_path_to_dar)
 
 
@@ -89,6 +87,3 @@ logging.debug('Working in {tmp_directory}'.format(tmp_directory=tmp_directory))
 get_source(url, tmp_directory)
 
 build_dar(daml_sdk_version, full_path_to_dar, tmp_directory)
-
-logging.info('Removing {tmp_directory}'.format(tmp_directory=tmp_directory))
-rmtree(tmp_directory)
